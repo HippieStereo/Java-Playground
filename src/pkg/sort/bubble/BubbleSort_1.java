524318f71db5a7b2ed5829b95850d94b553c946d
Hippie Stereo <stereobroo@yahoo.com>
2020-12-28 15:24:05 +0000

    Bubble Sort

---
+++ src/pkg/sort/bubble/BubbleSort_1.java
@@ -0,0 +1,52 @@
+package pkg.sort.bubble;
+
+import pkg.array.CreateArray;
+
+public class BubbleSort_1 {
+	
+	int tempValue;
+	
+	public void bubbleSort(int[] arrayIn, CreateArray createArray) {
+		
+		for(int j = arrayIn.length - 1; j > 1; j--) {
+			
+			for(int i = 0; i < arrayIn.length - 1; i++) {
+				
+				if(arrayIn[i] > arrayIn[i + 1]) {
+					
+					createArray.swapValuesArray(arrayIn, i, i + 1);
+					
+				}
+				
+			}
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+
+		CreateArray createArray = new CreateArray();
+		BubbleSort_1 bubbleSort_1 = new BubbleSort_1();
+
+		// - make array
+		int[] theArray = createArray.makeArray(10);
+		
+		createArray.generateRandomValues(theArray);
+		
+		createArray.printArray(theArray);
+		
+		// - execution time
+		long startTime = System.nanoTime();
+		
+		bubbleSort_1.bubbleSort(theArray, createArray);
+		
+		long endTime = System.nanoTime();
+		
+		createArray.printArray(theArray);
+		
+		System.out.println("EXECUTION TIME : " + (endTime - startTime) + " / nanoseconds");
+		
+	}
+
+}
---
+++ src/pkg/sort/bubble/BubbleSort_2.java
@@ -0,0 +1,62 @@
+package pkg.sort.bubble;
+
+import pkg.array.CreateArray;
+
+public class BubbleSort_2 {
+	
+	int tempValue;
+	
+	public void bubbleSort(int[] arrayIn, CreateArray createArray) {
+		
+		boolean isSorted = false;
+		
+		int latestNotSorted = arrayIn.length - 1;
+		
+		while(!isSorted) {
+			
+			isSorted = true;
+			
+			for(int i = 0; i < latestNotSorted; i++) {
+				
+				if(arrayIn[i] > arrayIn[i + 1]) {
+					
+					createArray.swapValuesArray(arrayIn, i, i + 1);
+					
+					isSorted = false;
+					
+				}
+				
+			}
+			
+			latestNotSorted--;
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+
+		CreateArray createArray = new CreateArray();
+		BubbleSort_2 bubbleSort_2 = new BubbleSort_2();
+
+		// - make array
+		int[] theArray = createArray.makeArray(10);
+		
+		createArray.generateRandomValues(theArray);
+		
+		createArray.printArray(theArray);
+		
+		// - execution time
+		long startTime = System.nanoTime();
+		
+		bubbleSort_2.bubbleSort(theArray, createArray);
+		
+		long endTime = System.nanoTime();
+		
+		createArray.printArray(theArray);
+		
+		System.out.println("EXECUTION TIME : " + (endTime - startTime) + " / nanoseconds");
+		
+	}
+
+}
