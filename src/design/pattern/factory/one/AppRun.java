9f2d508f9c576a78a364626471088e0bbffc9eb9
HippieStereo <stereobroo@yahoo.com>
2021-02-25 15:26:45 +0000

    Update AppRun.java
    Dynamic way ...

--- src/design/pattern/factory/one/AppRun.java
+++ src/design/pattern/factory/one/AppRun.java
@@ -1,14 +1,50 @@
 package design.pattern.factory.one;
 
+import java.util.Scanner;
+
 public class AppRun {
 
 	public static void main(String[] args) {
 		
 		// - Old way to create object
-		EnemyShip ufoShip = new UFOEnemyShip();
+//		EnemyShip ufoShip = new UFOEnemyShip();
+//		
+//		doStuffEnemy(ufoShip);
 		
-		doStuffEnemy(ufoShip);
+		// - Dynamic way ...
 		
+		EnemyShip theEnemyShip = null;
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		String enemyShipOption = "";
+		
+		System.out.println("What time of ship ? ( {U}FO / {R}ocket )");
+		
+		if(userInput.hasNextLine()) {
+			
+			enemyShipOption = userInput.nextLine();
+			
+		}
+		
+		if (enemyShipOption.equals("U")) {
+			
+			theEnemyShip = new UFOEnemyShip();
+			
+			doStuffEnemy(theEnemyShip);
+			
+		} else if (enemyShipOption.equals("R")) {
+			
+			theEnemyShip = new RocketEnemyShip();
+			
+			doStuffEnemy(theEnemyShip);
+			
+		} else {
+			
+			System.out.println("ERROR : \"" + enemyShipOption +  "\" is not a option ...");
+			
+		}
+				
 	}
 
 	private static void doStuffEnemy(EnemyShip anEnemyShip) {
