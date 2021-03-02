ae3c9ec091f53448956b96524e3e97973e34f67f
HippieStereo <stereobroo@yahoo.com>
2021-03-02 22:03:19 +0000

    Java 8 - Predicate
    Predicate to a method

---
+++ src/java8/predicate/joins/one/AppRun.java
@@ -0,0 +1,35 @@
+package java8.predicate.joins.one;
+
+import java.util.function.Predicate;
+
+public class AppRun {
+
+	public static void main(String[] args) {
+
+		int[] theArray = {10, 20, 30};
+		
+		Predicate<Integer> predicateInt = i -> i > 10;
+		
+		// Old way
+		for (int j = 0; j < theArray.length; j++) {
+			
+			System.out.println("Is " + theArray[j] + " greater than 10 ? -> " + predicateInt.test(theArray[j]));
+			
+		}
+		
+		// - Predicate in method
+		checkNumber(predicateInt, theArray);
+		
+	}
+	
+	static void checkNumber(Predicate<Integer> predicateInt, int[] theArray) {
+		
+		for (int i : theArray) {
+			
+			System.out.println("Is " + i + " greater than 10 ? -> " + predicateInt.test(i));
+			
+		}
+		
+	}
+
+}
